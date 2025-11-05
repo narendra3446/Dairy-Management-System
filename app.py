fae596ea7016619262ac357e6f173ba8b797d584
Narendra Tongaonkar <ntongaonkar21@gmail.com>
2025-11-06 00:27:02 +0530

    Fix Procfile for Render deployment

--- app.py
+++ app.py
@@ -59,4 +59,7 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    port = int(os.environ.get("PORT", 10000))  # Render provides PORT=10000
+    app.run(host="0.0.0.0", port=port)
+    
